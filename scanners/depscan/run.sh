3aa3bd01e1e7e0816664bf8296e6f9f65143625a
Sasa Djolic <sasa.djolic@gmail.com>
2025-07-16 01:52:56 -0700

    feat: complete depscan integration
    depscan can now be run by radarctl locally using the Eureka DevSecOps
    container image for depscan

--- scanners/depscan/run.sh
+++ scanners/depscan/run.sh
@@ -4,5 +4,5 @@
 # $3 - Path to the output folder where scan results should be stored
 
 set -e
-docker run --rm -v $1:/app -v $2:/input -v $3:/output ghcr.io/owasp-dep-scan/dep-scan depscan --src /app --reports-dir /output/depscan --report-name depscan.sarif --report-template /input/sarif.j2 2>&1
+docker run --rm -v $1:/app -v $2:/input -v $3:/output ghcr.io/eurekadevsecops/radar-depscan 2>&1
 cp $3/depscan/depscan.sarif $3/depscan.sarif
