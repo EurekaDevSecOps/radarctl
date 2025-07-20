e03168f27d685a1339c080fe7e48ae2b5c4e88b1
Sasa Djolic <sasa.djolic@gmail.com>
2025-07-20 01:13:40 -0700

    fix: clean up escalations option for the scan command

--- src/util/sarif/index.js
+++ src/util/sarif/index.js
@@ -1,15 +1,7 @@
 module.exports = {
-  transforms: {
-    escalate: require('./transforms/escalate'),
-    merge: require('./transforms/merge'),
-    normalize: require('./transforms/normalize')
-  },
-  analysis: {
-    load: require('./analysis/load'),
-    summarize: require('./analysis/summarize')
-  },
-  visualizations: {
-    display_findings: require('./visualizations/display_findings'),
-    display_totals: require('./visualizations/display_totals')
-  }
+  merge: require('./merge'),
+  escalate: require('./escalate'),
+  summarize: require('./summarize'),
+  display_findings: require('./display_findings'),
+  display_totals: require('./display_totals')
 }
