680b32b32e98891252c15c404ad967e705a1e2cf
Sasa Djolic <sasa.djolic@gmail.com>
2025-07-20 22:28:41 -0700

    fix: fix normalize transform and simplify summarization

--- src/util/sarif/transforms/normalize.js
+++ src/util/sarif/transforms/normalize.js
@@ -4,14 +4,17 @@ module.exports = (sarif, dir) => {
   for (const run of sarif.runs) {
     if (!run.results) continue
     for (const result of run.results) {
-      if (!result.locations) continue
+
+      // Find paths in the finding description and make them relative to the scan directory.
+      if (result?.message?.text) result.message.text = result.message.text.replace('/app/', '')
+
+      // Make all physical locations for the result relative to the scan directory.
       for (const location of result.locations) {
         if (!location.physicalLocation?.artifactLocation?.uri?.startsWith('/app')) continue
         let file = path.relative('/app', location.physicalLocation.artifactLocation.uri)
-        file = path.join(dir, file)
-        file = path.relative(process.cwd(), file)
         location.physicalLocation.artifactLocation.uri = file
       }
+
     }
   }
 }
