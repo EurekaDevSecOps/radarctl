95fde0a4ac01e23a82eefae9cc4e30d02ddc4b49
Sasha Djolic <s.djolic@eurekadevsecops.com>
2025-10-02 10:47:30 -0700

    fix: vulnerability paths should be relative to repo root (#30)
    Fixes PE-674

--- src/util/sarif/transforms/normalize.js
+++ src/util/sarif/transforms/normalize.js
@@ -3,34 +3,30 @@ module.exports = (sarif, dir, git, root) => {
   // Normalize findings.
   for (const run of sarif.runs) {
 
+    // Subfolder within the repo root where the scan took place:
+    const subfolder = path.relative(root, dir)
+
     // Record the source repo location and the relative target subfolder within the repo.
-    run.originalUriBaseIds = {
-     SOURCE: {
-        uri: git.repo.url.https,
-        description: {
-          text: "Source origin for the target being scanned (ie. git repo URL)."
-        }
-     },
-      TARGET: {
-        uri: `${path.relative(root, dir)}`,
-        uriBaseId: "SOURCE",
-        description: {
-          text: "Scan target (subfolder) within the source repo or folder."
-        }
+    run.properties = {
+      repository: {
+        type: 'git',
+        url: git.repo.url.https
       }
     }
+    if (subfolder) run.properties.includedirs = [ `${subfolder}` ]
 
-    // Make all physical locations for the result relative to the scan directory.
+    // Make all physical locations for the result relative to the repo root directory.
+    // (or if the root is not available then to the scan directory)
     if (!run.results) continue
     for (const result of run.results) {
       for (const location of result.locations) {
         if (location.physicalLocation?.artifactLocation?.uri?.startsWith('/app')) {
-          let file = path.relative('/app', location.physicalLocation.artifactLocation.uri)
+          let file = path.join(subfolder, path.relative('/app', location.physicalLocation.artifactLocation.uri))
           if (result?.message?.text) result.message.text = result.message.text.replace(location.physicalLocation.artifactLocation.uri, file)
           location.physicalLocation.artifactLocation.uri = file
         }
         else if (location.physicalLocation?.artifactLocation?.uri?.startsWith('/')) {
-          let file = path.relative('/', location.physicalLocation.artifactLocation.uri)
+          let file = path.join(subfolder, path.relative('/', location.physicalLocation.artifactLocation.uri))
           if (result?.message?.text) result.message.text = result.message.text.replace(location.physicalLocation.artifactLocation.uri, file)
           location.physicalLocation.artifactLocation.uri = file
         }
