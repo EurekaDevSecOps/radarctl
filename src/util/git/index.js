3047150465a62b518e5684eafc0cbbbefbc28256
Sasa Djolic <sasa.djolic@gmail.com>
2025-10-03 17:05:24 -0700

    fix(PE-680): filter out contributors with invalid emails
    Fixes PE-680

--- src/util/git/index.js
+++ src/util/git/index.js
@@ -25,13 +25,31 @@ function metadata(folder) {
     // Get the tags for the current commit.
     let tags = execSync('git tag --points-at HEAD', { cwd: folder }).toString().trim()
     tags = '["' + tags.split('\n').join('","') + '"]'
-    tags = JSON.parse(tags).filter(tag => tag)
+    try {
+      tags = JSON.parse(tags).filter(tag => tag)
+    }
+    catch (error) {
+      throw new Error(`failed parsing repo tags: ${error.message}`)
+    }
 
     // Get the list of unique repo contributors (authors and committers).
-    const template = '"{\\\"name\\\":\\\"%cn\\\",\\\"email\\\":\\\"%ce\\\"}%n{\\\"name\\\":\\\"%an\\\",\\\"email\\\":\\\"%ae\\\"}"'
+    const template = '%cn:%ce%n%an:%ae%n'
     let contributors = execSync(`git log --pretty=${template} | sort -u`, { cwd: folder }).toString().trim()
-    contributors = '[' + contributors.split('\n').join(',') + ']'
-    contributors = JSON.parse(contributors)
+    try {
+      contributors = contributors
+        .split('\n')
+        .map(c => {
+          const ne = (c + '').split(':')
+          return {
+            name: ne?.at(0) ?? '',
+            email: ne?.at(1) ?? ''
+          }
+        })
+        .filter(c => isValidEmail(c.email))
+    }
+    catch (error) {
+      throw new Error(`failed parsing repo contributors: ${error.message}`)
+    }
 
     const script = `MAX_LENGTH=4;
 git rev-list --abbrev=4 --abbrev-commit --all | \
@@ -113,6 +131,33 @@ function root(folder) {
       }
     }
   }
+}
+
+/**
+ * Thanks to:
+ * http://fightingforalostcause.net/misc/2006/compare-email-regex.php
+ * http://thedailywtf.com/Articles/Validating_Email_Addresses.aspx
+ * http://stackoverflow.com/questions/201323/what-is-the-best-regular-expression-for-validating-email-addresses/201378#201378
+ * https://en.wikipedia.org/wiki/Email_address  The format of an email address is local-part@domain, where the 
+ * local part may be up to 64 octets long and the domain may have a maximum of 255 octets.[4]
+ */
+function isValidEmail(email) {
+  if (!email) return false
+
+  const parts = email.split('@')
+  if (parts.length !== 2) return false
+
+  const account = parts[0]
+  if (account.length > 64) return false
+
+  const address = parts[1]
+  if (address.length > 255) return false
+
+  const domainParts = address.split('.')
+  if (domainParts.some((part) => part.length > 63 )) return false
+
+  const emailRE = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+  return emailRE.test(email)
 }
 
 module.exports = {
