dd1b0fca8909d4b82c43cb2034234dbbd782aebb
Jared Meit <j.meit@fwdsec.com>
2025-10-17 17:56:27 -0400

    Change root of /app from root to $HOME

--- scanners/veracode-sca/run.sh
+++ scanners/veracode-sca/run.sh
@@ -15,7 +15,7 @@ CFG_DIR=$(cd $2; pwd)
 OUT_DIR=$(cd $3; pwd)
 
 docker run --rm \
-    -v "${APP_DIR}":/app \
+    -v "${APP_DIR}":/home/luser/app \
     -v "${CFG_DIR}":/tmp/radar-input \
     -v "${OUT_DIR}":/tmp/radar-output \
     -e SRCCLR_API_TOKEN=${SRCCLR_API_TOKEN} \
