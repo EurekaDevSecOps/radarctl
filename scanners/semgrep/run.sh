4d174618aa908d5aa05e92d05f1e03b323ae05ca
Jared Meit <j.meit@fwdsec.com>
2025-10-17 16:53:32 -0400

    Semgrep runner

---
+++ scanners/semgrep/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Parameters:
+# $1 - Path to the source code folder that should be scanned
+# $2 - Path to the assets folder
+# $3 - Path to the output folder where scan results should be stored
+###
+
+
+# Expand relative paths
+APP_DIR=$(cd $1; pwd)
+CFG_DIR=$(cd $2; pwd)
+OUT_DIR=$(cd $3; pwd)
+
+docker run --rm \
+    -v "${APP_DIR}":/src \
+    -v "${CFG_DIR}":/tmp/radar-input \
+    -v "${OUT_DIR}":/tmp/radar-output \
+    -e SEMGREP_APP_TOKEN="${SEMGREP_APP_TOKEN}" \
+    semgrep/semgrep \
+    semgrep ci
