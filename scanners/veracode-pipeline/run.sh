8529272f7d3de12ead0aee04a94d832960ac3f9c
Sasha Djolic <sasa.djolic@gmail.com>
2026-02-23 09:53:29 -0800

    feat(scanners): correct the manifest for Veracode Pipeline scanner

--- scanners/veracode-pipeline/run.sh
+++ scanners/veracode-pipeline/run.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-###
-# Requirements:
-# (1) The repo has already been packaged for Veracode as per https://docs.veracode.com/r/compilation_packaging
-#       and stored into 'veracode-package.zip' at the root of the repo. It can also be manually defined by setting the
-#       VERACODE_ZIPFILE environment variable.
-# (2) Execute radar by prefixing the environment variables VERACODE_API_KEY_ID and VERACODE_API_KEY_SECRET.
-#       EXAMPLE: 
-#           $ VERACODE_API_KEY_ID=123456789 VERACODE_API_KEY_SECRET=a1b2c3d4e5f6 radar scan /path/to/repo
-# Optional:
-# (1) Include the environment variable VERACODE_ZIPFILE to define a different path and filename fo
-#       the /path/to/repo/veracode-pacakage.zip file.
-###
 # Parameters:
 # $1 - Path to the source code folder that should be scanned
 # $2 - Path to the assets folder
 # $3 - Path to the output folder where scan results should be stored
-###
+
+# Requirements:
+# (1) The repo has already been packaged for Veracode as per https://docs.veracode.com/r/compilation_packaging
+#     and stored into 'veracode-package.zip' at the root of the repo. It can also be manually defined by setting the
+#     VERACODE_ZIPFILE environment variable.
+# (2) Set environment variables VERACODE_API_KEY_ID and VERACODE_API_KEY_SECRET
+#     EXAMPLE: 
+#     $ VERACODE_API_KEY_ID=123456789 VERACODE_API_KEY_SECRET=a1b2c3d4e5f6 radar scan /path/to/repo
+#
+# Optional:
+# (3) Include the environment variable VERACODE_ZIPFILE to define a different path and filename for
+#     the /path/to/repo/veracode-pacakage.zip file.
 
 set -e
 
