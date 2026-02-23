8f44c04faefc6cd07cbd36062a80237dd79b953a
mathew-at-revela <mathew@revela.io>
2026-02-23 11:03:37 -0500

    fix(scans): clean up physical locations parsing

--- src/util/sarif/transforms/normalize.js
+++ src/util/sarif/transforms/normalize.js
@@ -20,7 +20,18 @@ module.exports = (sarif, dir, git, root) => {
     if (!run.results) continue
     for (const result of run.results) {
       for (const location of result.locations) {
-        if (location.physicalLocation?.artifactLocation?.uri?.startsWith('/app')) {
+        // Pattern matches managed scanner temporary job directories:
+        // Format: /app/jobs/{uuid}/repo-{timestamp}/ or jobs/{uuid}/repo-{timestamp}/
+        // Example: /app/jobs/830f53a2-5f0c-4565-a262-607dfcd4d5e1/repo-1771653645/
+        const MANAGED_SCANNER_JOB_PREFIX = /^(\/app\/)?jobs\/[a-f0-9-]+\/repo-\d+\//
+        
+        if (location.physicalLocation?.artifactLocation?.uri?.match(MANAGED_SCANNER_JOB_PREFIX)) {
+          let file = location.physicalLocation.artifactLocation.uri.replace(MANAGED_SCANNER_JOB_PREFIX, '')
+          if (subfolder) file = path.join(subfolder, file)
+          if (result?.message?.text) result.message.text = result.message.text.replace(location.physicalLocation.artifactLocation.uri, file)
+          location.physicalLocation.artifactLocation.uri = file
+        }
+        else if (location.physicalLocation?.artifactLocation?.uri?.startsWith('/app')) {
           let file = path.join(subfolder, path.relative('/app', location.physicalLocation.artifactLocation.uri))
           if (result?.message?.text) result.message.text = result.message.text.replace(location.physicalLocation.artifactLocation.uri, file)
           location.physicalLocation.artifactLocation.uri = file
