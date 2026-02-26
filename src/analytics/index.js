08e8e6d20ddd4b0d707c80bb00716b5add1db56d
cvu-fwdsec <c.vu@fwdsec.com>
2026-02-25 16:18:36 -0800

    chore: tweak help event name

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -6,7 +6,7 @@ const ANALYTICS_EVENTS = {
   scan_command_started: 'scan_command_started',
   local_scan_started: 'local_scan_started',
   import_started: 'import_started',
-  help_command_invoked: 'help_command_invoked',
+  help_invoked: 'help_invoked',
   cli_installed: 'cli_installed'
 }
 
