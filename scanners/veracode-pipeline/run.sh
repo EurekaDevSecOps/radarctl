6520884ad098162e6541a20995bd462456788fbd
Sasha Djolic <sasa.djolic@gmail.com>
2026-02-26 14:49:29 -0800

    feat: correct the script that runs veracode-pipeline scanner

--- scanners/veracode-pipeline/run.sh
+++ scanners/veracode-pipeline/run.sh
@@ -6,16 +6,36 @@
 # $3 - Path to the output folder where scan results should be stored
 
 # Requirements:
-# (1) The repo has already been packaged for Veracode as per https://docs.veracode.com/r/compilation_packaging
-#     and stored into 'veracode-package.zip' at the root of the repo. It can also be manually defined by setting the
-#     VERACODE_ZIPFILE environment variable.
-# (2) Set environment variables VERACODE_API_KEY_ID and VERACODE_API_KEY_SECRET
-#     EXAMPLE: 
-#     $ VERACODE_API_KEY_ID=123456789 VERACODE_API_KEY_SECRET=a1b2c3d4e5f6 radar scan /path/to/repo
+#
+# Environment variables VERACODE_API_KEY_ID and VERACODE_API_KEY_SECRET must be set:
+# [Veracode Platform](https://docs.veracode.com/r/c_api_credentials3#generate-api-credentials).
+#
+# EXAMPLE:
+# $ VERACODE_API_KEY_ID=123456789 VERACODE_API_KEY_SECRET=a1b2c3d4e5f6 radar scan /path/to/repo
 #
 # Optional:
-# (3) Include the environment variable VERACODE_ZIPFILE to define a different path and filename for
-#     the /path/to/repo/veracode-pacakage.zip file.
+#
+# (A) The repo has already been packaged for Veracode as per https://docs.veracode.com/r/compilation_packaging
+#     and stored into a ZIP file somewhere within the repo. Set the environment variable VERACODE_ZIPFILE to the
+#     path/to/repo/veracode-package.zip file, relative to the root folder of the repo.
+#
+# -or-
+#
+# (B) Set the environment variable VERACODE_PACKAGE_CMD to the command that can create the Veracode package ZIP.
+#     We will run the command from the root of the repo. It should create veracode-package.zip and save it into
+#     the root folder of the repo. We will and submit this ZIP to Veracode Pipeline for a scan.
+#
+#     Examples:
+#     export VERACODE_PACKAGE_CMD="zip -qr veracode-package.zip lib"
+#     export VERACODE_PACKAGE_CMD="npm run build && zip -qr veracode-package.zip dist"
+#     export VERACODE_PACKAGE_CMD="make && zip -qr veracode-package.zip out"
+#
+# -or-
+#
+# (C) Your project does not need a build step. Omit both VERACODE_ZIPFILE and VERACODE_PACKAGE_CMD. We will
+#     automatically ZIP up the repo, excluding any files referenced in .gitignore, and submit to Veracode Pipeline
+#     for a scan. This is the default action if you don't set VERACODE_ZIPFILE and VERACODE_PACKAGE_CMD.
+#     This is appropriate for interpreted languages (Javascript, Python, etc) that don't need to be compiled.
 
 set -e
 
@@ -24,12 +44,13 @@ APP_DIR=$(cd $1; pwd)
 CFG_DIR=$(cd $2; pwd)
 OUT_DIR=$(cd $3; pwd)
 
-docker run --rm \
-    -v "${APP_DIR}":/home/luser/app \
-    -v "${CFG_DIR}":/home/luser/input \
-    -v "${OUT_DIR}":/home/luser/output \
+# Veracode Pipeline only supports linux/amd64.
+docker run --platform linux/amd64 --rm \
+    -v "${APP_DIR}":/opt/eureka/radar/temp/repo \
+    -v "${CFG_DIR}":/opt/eureka/radar/temp/input \
+    -v "${OUT_DIR}":/opt/eureka/radar/temp/output \
     -e VERACODE_API_KEY_ID="${VERACODE_API_KEY_ID}" \
     -e VERACODE_API_KEY_SECRET="${VERACODE_API_KEY_SECRET}" \
     -e VERACODE_ZIPFILE="${VERACODE_ZIPFILE}" \
-    -e BUILD_DIR="/home/luser/app/${BUILD_DIR}" \
-    ghcr.io/eurekadevsecops/veracode-pipeline 2>&1
+    -e VERACODE_PACKAGE_CMD="${VERACODE_PACKAGE_CMD}" \
+    ghcr.io/eurekadevsecops/radar-veracode-pipeline 2>&1
