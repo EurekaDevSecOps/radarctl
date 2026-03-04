94cb726c0ca7a2fe45cc5606aa3bcf8b2b74eca4
CharlieHelps <charlie@charlielabs.ai>
2026-03-04 01:37:45 +0000

    docs(veracode): avoid inline secrets in example

--- scanners/veracode-sast/run.sh
+++ scanners/veracode-sast/run.sh
@@ -11,7 +11,8 @@
 # [Veracode Platform](https://docs.veracode.com/r/c_api_credentials3#generate-api-credentials).
 #
 # EXAMPLE:
-# $ VERACODE_API_KEY_ID=123456789 VERACODE_API_KEY_SECRET=somesecret radar scan /path/to/repo
+# $ VERACODE_API_KEY_ID=123456789 VERACODE_API_KEY_SECRET=REDACTED radar scan /path/to/repo
+# (Avoid inlining real secrets; prefer setting these via your CI/secret manager.)
 #
 # Optional:
 #
@@ -23,7 +24,7 @@
 #
 # (B) Set the environment variable VERACODE_PACKAGE_CMD to the command that can create the Veracode package ZIP.
 #     We will run the command from the root of the repo. It should create veracode-package.zip and save it into
-#     the root folder of the repo. We will submit this ZIP to Veracode Pipeline SAST scanner for a scan.
+#     the root folder of the repo. We will submit this ZIP to Veracode SAST scanner for a scan.
 #
 #     Examples:
 #     export VERACODE_PACKAGE_CMD="zip -qr veracode-package.zip lib"
@@ -33,8 +34,8 @@
 # -or-
 #
 # (C) Your project does not need a build step. Omit both VERACODE_ZIPFILE and VERACODE_PACKAGE_CMD. We will
-#     automatically ZIP up the repo, excluding any files referenced in .gitignore, and submit to Veracode Pipeline
-#     SAST scanner for a scan. This is the default action if you don't set VERACODE_ZIPFILE and VERACODE_PACKAGE_CMD.
+#     automatically ZIP up the repo, excluding any files referenced in .gitignore, and submit to Veracode SAST
+#     scanner for a scan. This is the default action if you don't set VERACODE_ZIPFILE and VERACODE_PACKAGE_CMD.
 #     This is appropriate for interpreted languages (Javascript, Python, etc) that don't need to be compiled.
 
 set -e
@@ -44,7 +45,7 @@ APP_DIR=$(cd $1; pwd)
 CFG_DIR=$(cd $2; pwd)
 OUT_DIR=$(cd $3; pwd)
 
-# Veracode SAST only supports linux/amd64.
+# The ghcr.io/eurekadevsecops/radar-veracode-sast image is currently published for linux/amd64 only.
 docker run --platform linux/amd64 --rm \
     -v "${APP_DIR}":/opt/eureka/radar/temp/repo \
     -v "${CFG_DIR}":/opt/eureka/radar/temp/input \
