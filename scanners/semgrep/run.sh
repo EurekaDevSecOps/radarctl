ab0244559831444ec8112ca2ac280266050f1972
Sasha Djolic <s.djolic@eurekadevsecops.com>
2026-06-10 13:22:41 -0700

    fix: correct semgrep image name and Cmd+C support (#105)

--- scanners/semgrep/run.sh
+++ scanners/semgrep/run.sh
@@ -8,15 +8,24 @@ set -e
 # $3 - Path to the output folder where scan results should be stored
 ###
 
-
 # Expand relative paths
 APP_DIR=$(cd $1; pwd)
 CFG_DIR=$(cd $2; pwd)
 OUT_DIR=$(cd $3; pwd)
 
-docker run --rm \
+trap cleanup TERM INT
+
+cleanup()
+{
+  PID=$(cat "${OUT_DIR}/semgrep.cid")
+  docker stop $PID
+  rm "${OUT_DIR}/semgrep.cid"
+  exit 1
+}
+
+docker run --cidfile "${OUT_DIR}/semgrep.cid" --rm \
     -v "${APP_DIR}":/home/luser/app \
     -v "${CFG_DIR}":/tmp/radar-input \
     -v "${OUT_DIR}":/tmp/radar-output \
     -e SEMGREP_APP_TOKEN="${SEMGREP_APP_TOKEN}" \
-    ghcr.io/eurekadevsecops/semgrep
+    ghcr.io/eurekadevsecops/radar-semgrep 2>&1
